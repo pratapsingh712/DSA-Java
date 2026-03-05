57d1ef85468303967cdab51ccd49adc1b6d3f2aa
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-05 18:43:34 +0530

    Character Data Types

---
+++ src/basics/CharacterDataTypes.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class CharacterDataTypes {
+    public static void main(String[] args) {
+
+        char ch = 's';
+
+        System.out.println(ch);
+
+        char ch1 = 'e';
+
+        System.out.println(ch1);
+
+    }
+}
