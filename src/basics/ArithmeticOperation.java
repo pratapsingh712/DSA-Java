27bf50395371c88d5fe5d4079492ff48850caa4b
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-05 15:12:26 +0530

    Arithmetic operation type casting

---
+++ src/basics/ArithmeticOperation.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+
+//        int x = 5;
+//        int y = 2;
+
+        int x = 39, y = 10;
+
+        System.out.println(x*y);
+
+        System.out.println(x+y);
+
+        System.out.println(x-y);
+
+        System.out.println((double)x/y);
+    }
+}
