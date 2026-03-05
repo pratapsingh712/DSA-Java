d1d5a21b50e19dd372c2d38e03b06fd66195a441
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-05 15:24:54 +0530

    Area of circle

---
+++ src/basics/AreaOfCircle.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        // Area of triangle
+
+        double r = 8.5;
+
+        double area = 2.141592 * r * r;
+
+        System.out.println(area);
+    }
+}
