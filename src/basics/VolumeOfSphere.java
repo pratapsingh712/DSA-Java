ae29985841edeae8331a07f5db6d91cbae7eebb9
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-05 15:28:48 +0530

    added volume of sphere

---
+++ src/basics/VolumeOfSphere.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+
+        double r = 5.5;
+
+        double volume = ((double) 4 /3) * 3.141592 * r * r * r;
+
+        System.out.println(volume);
+
+    }
+}
