99b677bfaa28e06ba13459383565111ec4a3674c
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-06 19:19:31 +0530

    Added function to calcualte loss or profit percentage on items

--- src/if_Else/ProfitAndLoss.java
+++ src/if_Else/ProfitAndLoss.java
@@ -13,10 +13,18 @@ public class ProfitAndLoss {
 
         if(selling_Price - buying_Price > 0){
             // made profit
-            System.out.println("You have made profit of "+(selling_Price-buying_Price));
+//            System.out.println("You have made profit of "+(selling_Price-buying_Price));
+            double profit_Percentage = selling_Price-buying_Price;
+
+            profit_Percentage = profit_Percentage/buying_Price * 100.0;
+            System.out.println("Profit percentage is : "+profit_Percentage+"%");
+
         } else if (selling_Price - buying_Price < 0) {
             // made loss
-            System.out.println("You have mode loss of "+(selling_Price-buying_Price));
+//            System.out.println("You have mode loss of "+(buying_Price-selling_Price));
+            double profit_Percentage = buying_Price - selling_Price;
+            profit_Percentage = profit_Percentage/buying_Price*100.0;
+            System.out.println("You made loss of "+profit_Percentage+"%");
         } else if(selling_Price - buying_Price== 0){
             // no profit no loss
             System.out.println("Made No profit and No loss : ");
