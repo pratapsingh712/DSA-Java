1f1b5adda3902d780a2ad1e7df2e047cffaa670d
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-06 18:58:37 +0530

    Added function that checks if real number is integer or double

--- src/if_Else/CheckInteger.java
+++ src/if_Else/CheckInteger.java
@@ -8,21 +8,29 @@ public class CheckInteger {
         System.out.println("Enter any positive number :");
         double num = sc.nextDouble();
 
+        // for natural numbers
         try{
             if(num<0){
                 throw new IllegalArgumentException("Negative Number is not allowed !");
             }else{
                 int x = (int)num;
-                if(num-x>0){
-                    System.out.println("Number is Double :");
-                }else{
+                if(num-x==0){
                     System.out.println("Number is Integer :");
+                }else{
+                    System.out.println("Number is Double :");
                 }
             }
         }catch (IllegalArgumentException e){
             System.out.println("Error : "+e.getMessage());
         }
 
+        // for real numbers
 
+        int numbers = (int) num;
+        if(num-(numbers) == 0){
+            System.out.println("It's a Integer :");
+        }else {
+            System.out.println("It's a Double :");
+        }
     }
 }
