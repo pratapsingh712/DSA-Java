374ff6ff35fd63f4b2c5b5d325ec11a9c5b00c7e
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-06 17:35:25 +0530

    BODMAS

---
+++ src/basics/Bodmas.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Bodmas {
+    public static void main(String[] args) {
+
+        // BODMAS says bracket > order > div > mul > add > sub
+
+        double x = 4.0*2/3; // for mul and div order in which they are executed does not matter
+
+        System.out.println(x);
+
+
+    }
+}
