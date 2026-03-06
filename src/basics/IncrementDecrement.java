99d26933cd65d3957f7ae921d6713e8e238e0aff
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-03-06 17:55:00 +0530

    Increment and Decrement Operators

---
+++ src/basics/IncrementDecrement.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int x = 10;
+//        System.out.println(x++); post increment
+       // System.out.println(++x); pre increment
+//        System.out.println(x);
+
+        int y = x++;
+
+        System.out.println(x+" "+y);
+    }
+}
